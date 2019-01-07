65a187d207a8ef65e1a2e1e2488c82f53ece2418
Elijah Bansley <elijahbansley@monroe.k12.wi.us>
2019-01-07 14:58:26 -0600

    Note conversion partially implemented

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import midi
 import os
+import time
 import glob
+
+notes = {
+	50: 294,
+	53: 349,
+	55: 391,
+	56: 415,
+	57: 440,
+	62: 587
+}
+
+print(notes)
+
+debug = True;
 
 # Takes the midi and generates beep() functions
 def generateBeeps(track):
 	# beepstring is the final string that will be returned
 	beepstring = "";
 	tracknum = 1
+
+	# If should debug, print track
+	print(track[tracknum])
+
 	# For each event in the 1st track of the midi, do tis function
 	for event in track[tracknum]:
 		# If its the start of a note, do...
@@ -14,7 +32,15 @@ def generateBeeps(track):
 			# Delay the code by the NoteOnEvent tick (which is equal to the amount of time since the last note)
 			beepstring += "delay(" + str(event.tick) + ");\n";
 			# Make a beep with the note (incorrect as of now) and the duration is the tick of the next event (hopefully the NoteOffEvent, should probably make sure of that)
-			beepstring += "beep(" + str(event.data[0]) + "," + str(track[tracknum][track[tracknum].index(event) + 1].tick) + ");\n";
+
+			note = 0;
+
+			if event.data[0] in notes:
+				note = notes[event.data[0]]
+			else:
+				note = event.data[0]
+
+			beepstring += "beep(" + str(note) + "," + str(track[tracknum][track[tracknum].index(event) + 1].tick) + ");\n";
 	# Give the code the final string
 	return beepstring
 
@@ -63,7 +89,8 @@ def MakeCode(midname):
 
 	# Writes the final string to a file so we don't have to output it in the terminal
 	if os.path.exists("result.txt"): 
-		os.remove("result.txt") 
+		os.remove("result.txt")
+		time.sleep(1) 
 	else: 
 		print("No result.txt")
 	text_file = open("result.txt", "w")
